906cb02809e23f0bdecb78e7a70589a2eb304f54
tangemily <tang.s.emily@gmail.com>
2022-04-15 23:14:59 -0400

    mongo atlas

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,37 +1,27 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import tuitsDao from "../tuits/tuits-dao.js";
 
-const createTuit = (req, res) => {
-    const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 0;
-    newTuit.dislikes = 0;
-    newTuit.comments = 0;
-    newTuit.retweets = 0;
-    newTuit.liked = false;
-    newTuit.disliked = false;
-    newTuit.handle = "Emily";
-    newTuit.postedBy = {};
-    newTuit.postedBy.username = "Emily";
-    newTuit.logo = "https://pbs.twimg.com/media/ETfL-nIWkAI8x7l.jpg";
-
-    tuits = [newTuit, ...tuits]
-    res.json(newTuit);
-}
-const findAllTuits = (req, res) => {
+const findAllTuits = async (req, res) => {
+    const tuits = await tuitsDao.findAllTuits()
     res.json(tuits);
 }
-const updateTuit = (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const updatedTuit = req.body;
-    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
-    res.sendStatus(200);
+
+const createTuit = async (req, res) => {
+    const newTuit = req.body;
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 }
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+    res.send(status);
+}
+
+const updateTuit = async (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updatedTuit = req.body;
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
+    res.send(status);
 }
 
 export default (app) => {
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
 helloController(app);
 userController(app);
 tuitsController(app);
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.get('/', (req, res) => {
+    res.send('Welcome to Full Stack Development!')
+})
 app.listen(process.env.PORT || 4000);
